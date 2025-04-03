49efc6fcb497ddf55d3bd18500390744e336e6bc
Jainish1808 <135621871+Jainish1808@users.noreply.github.com>
2025-04-03 10:27:47 +0530

    Delete heart directory

--- heart/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import HeartDisease
-from .models import StrokePrediction
-
-# Register your models here.
-admin.site.register(HeartDisease)
-admin.site.register(StrokePrediction)
